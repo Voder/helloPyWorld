ba20bed81e32b12b31e0dfd6877ee019496b566e
Volker Aufschild <mail@volker-aufschild.de>
2014-05-20 19:32:11 +0200

    Sort by class method

--- sort_person.py
+++ sort_person.py
@@ -11,21 +11,24 @@ class Person(object):
 	def __repr__(self):
 		return 'Person(vorname=' + self.vorname + ', name=' + self.name + ', gebDat=' + self.gebDat + ')'
 
+	def sortByVorname(self):
+		return self.vorname
+
 def main():
     p1 = Person('Hans', 'Wurst', '01.01.1990')
     # print(p1)
-    p2 = Person('Hans', 'Kaese', '01.06.1995')
+    p2 = Person('Sepp', 'Kaese', '01.06.1995')
     # print(p2)
-    p3 = Person('Hans', 'Brot', '01.03.1970')
+    p3 = Person('Karl', 'Brot', '01.03.1970')
     # print(p3)
-    p4 = Person('Hans', 'Bier', '01.01.1980')
+    p4 = Person('John', 'Bier', '01.01.1980')
     # print(p4)
 
     persons = [p1, p2, p3, p4]
     print (persons)
 
     print('Sorted')
-    print (sorted (persons, key=sortByName))
+    print (sorted (persons, key=Person.sortByVorname))
 
 def sortByName(person):
 	return person.name
